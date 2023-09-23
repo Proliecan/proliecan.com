3d799a65ed99466f6ad96b404b766be83e06b2ad
Proliecan <65495509+Proliecan@users.noreply.github.com>
2023-09-23 18:11:28 +0200

    Read and use Config

--- src/server.go
+++ src/server.go
@@ -1,28 +1,97 @@
 package main
 
 import (
+	"fmt"
+	"io/ioutil"
 	"log"
 	"net/http"
 	"os"
 	"os/signal"
 	"syscall"
+
+	yaml "gopkg.in/yaml.v3"
 )
+
+var verbose bool = false
 
 func main() {
 	done := make(chan os.Signal, 1)
 	signal.Notify(done, os.Interrupt, syscall.SIGINT, syscall.SIGTERM)
+
+	// read config file path from command line
+	var configPath string = parseArgs()
+	// read config file
+	config := Config{}
+	config.readFromFile(configPath)
 
 	log.Println("Starting server...")
 	initServer()
 
 	// start server
 	go func() {
-		log.Println("Server Started")
-		log.Fatal(http.ListenAndServe(":8080", nil))
+		log.Fatal(http.ListenAndServe(fmt.Sprintf("%s:%d", config.Domain, config.Port), nil))
 	}()
+	// print server info
+	log.Println("Server started at", colorize(config.Domain, Yellow)+":"+colorize(fmt.Sprintf("%d", config.Port), Yellow))
 
 	<-done
 	log.Println("\nServer Stopped gracefully")
+}
+
+type Config struct {
+	Domain string
+	Port   int
+}
+
+func (c *Config) readFromFile(path string) {
+	if verbose {
+		log.Println("Reading config file from", path)
+	}
+
+	yamlFile, err := ioutil.ReadFile(path)
+	if err != nil {
+		log.Printf("yamlFile.Get err   #%v ", err)
+	}
+	err = yaml.Unmarshal(yamlFile, c)
+	if err != nil {
+		log.Fatalf("Unmarshal: %v", err)
+	}
+
+	if verbose {
+		fmt.Printf("Domain: %s\n", c.Domain)
+		fmt.Printf("Port: %d\n", c.Port)
+	}
+}
+
+func parseArgs() string {
+	bin := os.Args[0]
+	// find pos of -c
+	var configPath string
+	for i, arg := range os.Args {
+		if arg == "-c" {
+			if i+1 < len(os.Args) {
+				configPath = os.Args[i+1]
+				continue
+			}
+		}
+		if arg == "-v" {
+			verbose = true
+			continue
+		}
+	}
+
+	if configPath == "" {
+		printUsage(bin)
+		os.Exit(1)
+	}
+
+	return configPath
+}
+
+func printUsage(bin string) {
+	log.Println(colorize("Usage:", Red),
+		colorize(bin, Yellow), colorize("-c", Cyan), colorize("<path to config file>", Cyan),
+		colorize("[-v]", Blue))
 }
 
 func initServer() {
