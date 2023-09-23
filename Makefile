43dacd1414e4acdd7e5f2acc254e88e0a6bc994d
Proliecan <65495509+Proliecan@users.noreply.github.com>
2023-09-23 17:48:00 +0200

    Basic webserver graceful stop

--- Makefile
+++ Makefile
@@ -1,26 +1,2 @@
-# go compiler
-GO=go
-
-# src dir
-SRC_DIR=src
-
-# build dir
-BUILD_DIR=out
-# binary name
-BINARY_NAME=server
-
-# gofiles
-GOFILES=$(shell find $(SRC_DIR) -name "*.go")
-MAIN_FILE=$(SRC_DIR)/server.go
-
-# run
-run: build
-	$(BUILD_DIR)/$(BINARY_NAME)
-
-# build
-build: $(GOFILES)
-	$(GO) build -o $(BUILD_DIR)/$(BINARY_NAME) $(MAIN_FILE)
-
-# clean
-clean:
-	rm -rf $(BUILD_DIR)/*
+all:
+	@make -C src
--- src/server.go
+++ src/server.go
@@ -1,9 +1,66 @@
 package main
 
 import (
-	"fmt"
+	"log"
+	"net/http"
+	"os"
+	"os/signal"
+	"syscall"
 )
 
 func main() {
-	fmt.Println("Starting server...")
+	done := make(chan os.Signal, 1)
+	signal.Notify(done, os.Interrupt, syscall.SIGINT, syscall.SIGTERM)
+
+	log.Println("Starting server...")
+	initServer()
+
+	// start server
+	go func() {
+		log.Println("Server Started")
+		log.Fatal(http.ListenAndServe(":8080", nil))
+	}()
+
+	<-done
+	log.Println("\nServer Stopped gracefully")
+}
+
+func initServer() {
+	// handle /
+	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+		log.Println(colorize(r.RemoteAddr, Yellow),
+			colorize(">", Red), r.Method, r.URL.Path)
+		log.Println(colorize(r.RemoteAddr, Yellow),
+			colorize("<", Green), "Status:", http.StatusOK)
+		w.WriteHeader(http.StatusOK)
+	})
+}
+
+// colorize string
+type ColorCode string
+
+const (
+	Reset     ColorCode = "\033[0m"
+	Red       ColorCode = "\033[31m"
+	Green     ColorCode = "\033[32m"
+	Yellow    ColorCode = "\033[33m"
+	Blue      ColorCode = "\033[34m"
+	Magenta   ColorCode = "\033[35m"
+	Cyan      ColorCode = "\033[36m"
+	White     ColorCode = "\033[37m"
+	RedBG     ColorCode = "\033[41m"
+	GreenBG   ColorCode = "\033[42m"
+	YellowBG  ColorCode = "\033[43m"
+	BlueBG    ColorCode = "\033[44m"
+	MagentaBG ColorCode = "\033[45m"
+	CyanBG    ColorCode = "\033[46m"
+	WhiteBG   ColorCode = "\033[47m"
+	Bold      ColorCode = "\033[1m"
+	Dim       ColorCode = "\033[2m"
+	Italic    ColorCode = "\033[3m"
+	Underline ColorCode = "\033[4m"
+)
+
+func colorize(s string, color ColorCode) string {
+	return string(color) + s + string(Reset)
 }
